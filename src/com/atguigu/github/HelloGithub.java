f4c3a3f672584557d89934c008382e8314e6e8e1
wangb1022 <2386090244@qq.com>
2016-03-19 02:31:32 +0800

    update HelloGithub.java

--- src/com/atguigu/github/HelloGithub.java
+++ src/com/atguigu/github/HelloGithub.java
@@ -5,6 +5,7 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		System.out.println("hello,github!");
 		System.out.println("hello,github2!");
+		System.out.println("hello,github3!");
 	}
 	
 }
